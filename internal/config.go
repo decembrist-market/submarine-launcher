b52d7933df819bf37053c42d60d1283150b6b4c6
AViDia <157277809+AViDia-Evgenika@users.noreply.github.com>
2025-07-29 01:34:01 +0300

    initial commit (#1)
    * initial commit
    
    * Update game configuration and .gitignore
    
    ---------
    
    Co-authored-by: Decembrist IT <decembrist.org@gmail.com>

---
+++ internal/config.go
@@ -0,0 +1,9 @@
+package internal
+
+var (
+	GameFolderName   = "SubmarineGame"
+	RemoteVersionURL = "https://static.decembrist.org/submarine-game/version.yaml"
+	VersionFileName  = "version.yaml"
+	GameFileName     = "submarine"
+	ArchiveURL       = "https://static.decembrist.org/submarine-game/submarine.zip"
+)
---
+++ internal/game.go
@@ -0,0 +1,58 @@
+﻿package internal
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+func getExecutableName(baseName string) string {
+	if runtime.GOOS == "windows" {
+		return baseName + ".exe"
+	}
+	return baseName
+}
+func runExecution(path string) error {
+	cmd := exec.Command(path)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Start()
+}
+
+func TryRunGame(dataDir string) {
+	fmt.Print("Запуск игры...\n")
+	gameFilePath := filepath.Join(dataDir, getExecutableName(GameFileName))
+	if _, err := os.Stat(gameFilePath); err == nil {
+		err := runExecution(gameFilePath)
+		if err != nil {
+			ShowExitMessage(Error, "Ошибка при запуске игры:", err)
+			return
+		}
+	} else {
+		ShowExitMessage(Error, "Файл игры не найден:", fmt.Errorf("путь: %s", gameFilePath))
+		return
+	}
+	fmt.Println("Игра запущена")
+	os.Exit(0)
+}
+
+func GetGameDirection(launcherPath string) (string, error) {
+	launcherDirPath := filepath.Dir(launcherPath)
+	var gameDirPath string
+	if filepath.Base(launcherDirPath) == GameFolderName {
+		gameDirPath = launcherDirPath
+	} else {
+		gameDirPath = filepath.Join(launcherDirPath, GameFolderName)
+		if _, err := os.Stat(gameDirPath); os.IsNotExist(err) {
+			err := os.Mkdir(gameDirPath, 0755)
+			if err != nil {
+				return "", err
+			}
+			fmt.Println("Папка игры создана.")
+		}
+	}
+	return gameDirPath, nil
+}
